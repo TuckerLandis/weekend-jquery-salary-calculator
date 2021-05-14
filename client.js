e5253adaee04f30e6414c01a3ab860fb768b5209
TuckerLandis <landistuckerc@gmail.com>
2021-05-14 16:08:47 -0500

    appending rows working

--- client.js
+++ client.js
@@ -1,5 +1,8 @@
 console.log('JS load');
 
+let employees = [];
+
+
 $(document).ready(function() {
     console.log('JQ Load');
     
@@ -7,12 +10,10 @@ $(document).ready(function() {
     // click listeners
     $('#submitButton').on("click", submitBlast)
 
-
-
 });
 
 function submitBlast () {
-    // get employye object from input boxes
+    // get employee object from input boxes
     let employeeObj = {
         firstName: $('#firstNameInput').val(),
         lastName: $('#lastNameInput').val(),
@@ -20,6 +21,29 @@ function submitBlast () {
         jobTitle: $('#jobTitleInput').val(),
         annualSalary: $('#annualSalaryInput').val()
     }
-    console.log(employeeObj);
+    // console.log(employeeObj);
+    employees.push(employeeObj);
+    // console.log(employees);
+
+    $('#formOutput').append(`
+    <tr>
+                <td>${employeeObj.firstName}</td>
+                <td>${employeeObj.lastName}</td>
+                <td>${employeeObj.employeeId}</td>
+                <td>${employeeObj.jobTitle}</td>
+                <td>${employeeObj.annualSalary}</td>
+              </tr>
+    `)
+
+
+    // create and append new line with values of object 
+    
+    
     
 }
+
+
+// to do
+
+// append object properties to >> [ array? ] >> rows of table   /// maybe dont need array
+// append rows to dom 
