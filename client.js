2224be6d0288fdc5797c952a184ae2c8aca6ece3
TuckerLandis <landistuckerc@gmail.com>
2021-05-15 17:18:09 -0500

    figured out stretch rewriting client

--- client.js
+++ client.js
@@ -3,22 +3,22 @@ console.log('JS load');
 let employees = [];
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     console.log('JQ Load');
-    
+
 
     // click listeners
     $('#submitButton').on("click", submitEmp);
-    $('#submitButton').on("click", calculateMonthly);
+    $('#submitButton').on("click", newerCalcMonthly);
     //$('#submitButton').on("click", displayMonthly)
     //add listener for delete button here?
-    $('.tableOfStuff').on("click", ".deleter" , theDeleter); // 2nd arg issues? // had to 
+    $('.tableOfStuff').on("click", ".deleter", theDeleter);
 
 
 
 });
 
-function submitEmp () {
+function submitEmp() {
     // get employee object from input boxes
     let employeeObj = {
         firstName: $('#firstNameInput').val(),
@@ -31,9 +31,9 @@ function submitEmp () {
     employees.push(employeeObj);
     $('input').val('') // empties all inputs change if necessary
     // console.log('Employees: 'employees); // expect 1 new 
-    
+
     // blasts employee to DOM
-    $('#formOutput').append( ` 
+    $('#formOutput').append(` // open
    
    
         <tr class="toBeDeleted">
@@ -41,53 +41,117 @@ function submitEmp () {
                 <td>${employeeObj.lastName}</td>
                 <td>${employeeObj.employeeId}</td>
                 <td>${employeeObj.jobTitle}</td>
-                <td>${employeeObj.annualSalary}</td>
+                <td class="salaries">${employeeObj.annualSalary}</td>
                 <td> <button class="deleter btn btn-danger">Delete</button> </td>
             
               </tr>
     `) // close new employee
-    
+
 } // end submit employee
 
 
 
-function calculateMonthly() {
-console.log('in Calc Monthly');
-let monthlyCost = 0;
-    for(i = 0; i < employees.length; i++) {
-        
-        monthlyCost += (employees[i].annualSalary) / 12;
-        monthlyCost = Math.round(monthlyCost)
-        console.log(monthlyCost); // 
-        
-    } // end loop
-    $('#monthlyOut').text(monthlyCost) 
 
-    if (monthlyCost > 20000) {
-        $('body').addClass('redAlert');
-        console.log('RED ALERT');
-        
-        
-        
-    }
+
+// function calculateMonthly() {
+
+// console.log('in Calc Monthly');
+// let monthlyCost = 0;
+//     for(i = 0; i < employees.length; i++) {
+
+//         monthlyCost += (employees[i].annualSalary) / 12;
+//         monthlyCost = Math.round(monthlyCost)
+//         console.log(monthlyCost); // 
+
+//     } // end loop
+//     $('#monthlyOut').text(monthlyCost) 
+
+//     if (monthlyCost > 20000) {
+//         $('body').addClass('redAlert');
+//         console.log('RED ALERT');
+
+
+
+//     }
+
+// }
+
+
+function theDeleter() {
+    console.log('Why u no delet');
+    console.log($(this));
+    $(this).closest('tr').remove();
+    newerCalcMonthly();
+    // find a variable based off of class = annual salary row text
+    // make sure this is number
+
 
 }
 
 
-function theDeleter () {
-    console.log('Why u no delet');
-    console.log($(this));
-    
-    $(this).closest('tr').remove();
-} // ???????
 
 
-// to do display some effect on click
-// when over 20k, display flashing red and audible siren
-// <td> <button type="button" class="deleter btn btn-danger">Delete</button> </td>
+
+// let salaryObj = ('.salaries');
+// salaryObj.each( function(i) { 
+//     this
+//  } )
+
+// salaryArray = $('.salaries').closest('td').text() //.closest().text();
+// console.log(salaryArray);
+
+// console.log($('.salaries').text())
 
 
-// this closet tr
 
 
-// the table on click #deleteButton
+let salaryArray = [];
+
+
+// returns undefined
+// function newCalcMonthly() {
+//     // target all elements class annual salary 
+//     let salaryObj = $('.salaries').text();
+//     console.log('salary obj', salaryObj);
+
+
+//     let sum = 0;
+
+//     //push those to a new array
+//     for(let salary of salaryObj ) {
+//         salaryArray.push(salary);
+//     }
+//     // iterate on that array and add
+//     for(i = 0; i < salaryArray.length; i++ ) {
+
+//         sum += salaryArray[i];
+//         console.log(sum);
+//     }
+//     console.log(sum);
+
+//     // add them / 12
+//     // append them
+// }
+
+console.log(newCalcMonthly());
+
+
+console.log($('.salaries'));
+
+
+function newerCalcMonthly() {
+    let sum = 0;
+    $('.salaries').each(function () {
+        sum += parseFloat($(this).text());
+    }); // end inner
+    console.log(sum);
+    sum = sum / 12;
+    $('#monthlyOut').text(sum);
+
+}; // end function
+
+
+// take inputs to page
+// get salaries text from page
+// run calc
+// delete > run calc
---
+++ newclient.js
@@ -0,0 +1,11 @@
+//page ready
+
+
+
+
+
+// function to add row > calculate
+
+// function to calculate
+
+// function to delete > calculate
